c8891843c3a0d46fcb8c1af0f1bdd08cb79cc0d0
jorge.picado.abaqo <jorge.picado@abaqo.tech>
2021-12-02 20:16:05 -0600

    Script sql para corregir nombre de las reuniones de Scrum

---
+++ CORRECCION_NOMBRE_REUNIONES.sql
@@ -0,0 +1,5 @@
+﻿select * from sc_spring_meeting_type;
+
+UPDATE  sc_spring_meeting_type SET nombre='SPRINT DAILY' WHERE id_meeting_type=1;
+UPDATE  sc_spring_meeting_type SET nombre='SPRINT REVIEW' WHERE id_meeting_type=2;
+UPDATE  sc_spring_meeting_type SET nombre='SPRINT RETROSPECTIVE' WHERE id_meeting_type=3;
